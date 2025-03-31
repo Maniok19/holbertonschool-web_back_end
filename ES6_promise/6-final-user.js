c1963b5dd64588d5b87e80a6ebb8d470c372241f
Maniok19 <mano19029@hotmail.fr>
2025-03-31 23:24:26 +0200

    feat: refactor promise handling functions and add new utility functions

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
-import signUpUser from "./4-user-promise"
-import uploadPhoto from "./5-photo-reject"
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-    const array = await Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    return array
+  const array = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return array;
 }
