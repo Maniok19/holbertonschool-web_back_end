521879becb1de86d5deab9569bfdcfcdebbf19b5
Maniok19 <mano19029@hotmail.fr>
2025-04-23 11:55:19 +0200

    refactor: improve formatting of element_length function for better readability

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -3,7 +3,8 @@ import typing
 """rock bap dooda diwap diwap diwap"""
 
 
-def element_length(lst: typing.Iterable[typing.Sequence])\
-      -> typing.List[typing.Tuple[typing.Sequence, int]]:
+def element_length(
+    lst: typing.Iterable[typing.Sequence]
+) -> typing.List[typing.Tuple[typing.Sequence, int]]:
     """rock bap dooda diwap diwap diwap"""
     return [(i, len(i)) for i in lst]
