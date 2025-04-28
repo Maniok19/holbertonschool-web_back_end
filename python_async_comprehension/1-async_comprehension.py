9eb1a01156d1bb4c0c463940aff3bf504c938cf1
Maniok19 <mano19029@hotmail.fr>
2025-04-28 13:25:24 +0200

    refactor: update async_comprehension function return type hint to list

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """Async Comprehension module blabla blabl blabla blabl blabl"""
-from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> list:
     """Async Comprehension module blabla blabl blabla blabl blabl"""
     lif = []
     async for i in async_generator():
