dac35dad3b9a7e38d1e3db74e680a447991e3877
Maniok19 <mano19029@hotmail.fr>
2025-04-28 13:26:59 +0200

    refactor: update async_comprehension return type hint to use typing.List

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Async Comprehension module blabla blabl blabla blabl blabl"""
+import typing
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list:
+async def async_comprehension() -> typing.List[float]:
     """Async Comprehension module blabla blabl blabla blabl blabl"""
     lif = []
     async for i in async_generator():
