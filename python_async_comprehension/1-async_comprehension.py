8bf23a3dc0ee64a966f52dfb0e31f413497584a7
Maniok19 <mano19029@hotmail.fr>
2025-04-28 12:45:13 +0200

    feat: update async_comprehension function return type to list

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -6,7 +6,7 @@ async_generator = __import__('0-async_generator').async_generator
 import typing
 
 
-async def async_comprehension() -> typing.List[float]:
+async def async_comprehension() -> list:
     """
     Collects 10 random numbers using async comprehension
     Returns: List of 10 random float numbers
