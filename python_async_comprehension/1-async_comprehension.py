98a2563e002964ab802dc35bfe5267b46387379d
Maniok19 <mano19029@hotmail.fr>
2025-04-28 11:28:22 +0200

    feat: update async_comprehension function to use typing.List for type hinting

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,9 +3,10 @@
 Async Comprehension module
 """
 async_generator = __import__('0-async_generator').async_generator
+import typing
 
 
-async def async_comprehension() -> list:
+async def async_comprehension() -> typing.List[float]:
     """
     Collects 10 random numbers using async comprehension
     Returns: List of 10 random float numbers
