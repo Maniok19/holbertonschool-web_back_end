687a03ca9c8339d515c6a8dcbc59f87163bc908c
Maniok19 <mano19029@hotmail.fr>
2025-04-28 13:31:47 +0200

    refactor: improve async_comprehension function and update module docstring

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-"""Async Comprehension module blabla blabl blabla blabl blabl"""
-import typing
+"""
+Module for async_comprehension
+"""
+
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> typing.List[float]:
-    """Async Comprehension module blabla blabl blabla blabl blabl"""
-    lif = []
-    async for i in async_generator():
-        lif.append(i)
-    return lif
+async def async_comprehension() -> List[float]:
+    """
+    Collects 10 random numbers using async comprehension over async_generator
+    Returns: List of 10 random float numbers
+    """
+    return [i async for i in async_generator()][:10]
