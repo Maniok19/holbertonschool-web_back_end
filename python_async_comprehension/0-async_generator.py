c5c6398f2246a42f69e888e81549658b5b3ed3c8
Maniok19 <mano19029@hotmail.fr>
2025-04-28 12:59:35 +0200

    try to fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import asyncio
 import random
-from typing import AsyncGenerator
+import typing
 """Module that contains an asynchronous generator"""
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> typing.AsyncGenerator[float, None]:
     """Coroutine that yields a random number between 0 and 10.
     The coroutine will loop 10 times, and wait 1 second each time."""
     for _ in range(10):
--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,10 +2,11 @@
 """
 Async Comprehension module
 """
+import typing
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list:
+async def async_comprehension() -> typing.List[float]:
     """
     Collects 10 random numbers using async comprehension
     Returns: List of 10 random float numbers
