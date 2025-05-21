bb984b88bf5c5af6591e931896abfa4800bc98f8
Maniok19 <mano19029@hotmail.fr>
2025-05-21 13:13:02 +0200

    refactor: remove countStudents function implementation and retain import for external use

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,38 +1,5 @@
-async function countStudents(path) {
-  const fs = require('fs').promises;
-  try {
-    const data = await fs.readFile(path, 'utf-8');
-
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
-
-    const students = lines.slice(1);
-
-    let result = (`Number of students: ${students.length}\n`);
-
-    const fieldGroups = {};
-
-    students.forEach((student) => {
-      const fields = student.split(',');
-      const firstName = fields[0];
-      const field = fields[3];
-
-      if (!fieldGroups[field]) {
-        fieldGroups[field] = [];
-      }
-      fieldGroups[field].push(firstName);
-    });
-    for (const field in fieldGroups) {
-      const studentsList = fieldGroups[field].join(', ');
-      result += (`Number of students in ${field}: ${fieldGroups[field].length}. List: ${studentsList}\n`);
-    }
-    result = result.trim();
-    return result;
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
-}
-
 const express = require('express');
+const countStudents = require('./3-read_file_async');
 
 const app = express();
 app.get('/', (req, res) => {
