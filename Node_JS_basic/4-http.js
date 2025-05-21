0faec0a28649d78da979d6c46cf5d79777f0952b
Maniok19 <mano19029@hotmail.fr>
2025-05-21 10:01:03 +0200

    refactor: replace res.write and res.end with res.send for improved response handling

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -2,7 +2,7 @@ const app = require('http');
 
 app.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello Holberton School!');
-  res.end();
-}).listen(1245);
+  res.send('Hello Holberton School!');
+})
+app.listen(1245);
 module.exports = app;
