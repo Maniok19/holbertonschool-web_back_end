ea9f0f60b6465878e6c4dd97ede182967b03d526
Maniok19 <mano19029@hotmail.fr>
2025-05-21 13:15:38 +0200

    refactor: update /students route to use promise handling for countStudents function

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -3,13 +3,19 @@ const countStudents = require('./3-read_file_async');
 
 const app = express();
 app.get('/', (req, res) => {
+  res.set('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
-app.get('/students', async (req, res) => {
-  res.set('Content-Type', 'text/plain');
-  const database = process.argv[2];
-  const list = await countStudents(database);
-  res.send(`This is the list of our students\n${list}`);
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2])
+    .then((data) => {
+      res.set('Content-Type', 'text/plain');
+      res.send(`This is the list of our students\n${data}`);
+    })
+    .catch((error) => {
+      res.set('Content-Type', 'text/plain');
+      res.send(`This is the list of our students\n${error.message}`);
+    });
 });
 app.listen(1245);
 module.exports = app;
