69219997f7b44e54d6331b0f7ef852204889642e
Maniok19 <mano19029@hotmail.fr>
2025-05-21 12:56:24 +0200

    refactor: streamline countStudents function and improve server response handling

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,44 +1,9 @@
-const fs = require('fs').promises;
-
-async function countStudents(path) {
-  try {
-    const data = await fs.readFile(path, 'utf-8');
-
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
-
-    const students = lines.slice(1);
-
-    let result = (`Number of students: ${students.length}\n`);
-
-    const fieldGroups = {};
-
-    students.forEach((student) => {
-      const fields = student.split(',');
-      const firstName = fields[0];
-      const field = fields[3];
-
-      if (!fieldGroups[field]) {
-        fieldGroups[field] = [];
-      }
-      fieldGroups[field].push(firstName);
-    });
-    for (const field in fieldGroups) {
-      if (Object.prototype.hasOwnProperty.call(fieldGroups, field)) {
-        const studentsList = fieldGroups[field].join(', ');
-        result += (`Number of students in ${field}: ${fieldGroups[field].length}. List: ${studentsList}\n`);
-      }
-    }
-    result = result.trim();
-    return result;
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
-}
 const http = require('http');
+const countStudents = require('./3-read_file_async');
 
 const app = http.createServer(async (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     const database = process.argv[2];
@@ -48,6 +13,7 @@ const app = http.createServer(async (req, res) => {
     res.end();
   }
   res.end();
-}).listen(1245);
+});
+app.listen(1245);
 
 module.exports = app;
